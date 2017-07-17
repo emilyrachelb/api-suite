8a7962c9fbc45d9aa9ac3c5275b986b26879de00
markwatneyy <samanthabelnavis@gmail.com>
2017-07-17 16:10:52 -0400

    really basic api test stuff

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000;
+  mongoose = require('mongoose'),
+  Task = require('./api/models/app.js'),
+  bodyParser = require('body-parser');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/rachelapi');
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+app.use(function(req, res) {
+  res.status(404).send({url: req.originalUrl + ' not found'})
+});
+
+var routes = require('./api/routes/tasks.js');
+routes(app);
 
 app.listen(port);
 
 console.log('API server started on: ' + port);
- 
